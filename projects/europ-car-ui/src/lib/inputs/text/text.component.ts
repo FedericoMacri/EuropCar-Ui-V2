a6701e21303b9cbba5510c3c229722fe7eeade8b
Federico <federico.macri27@gmail.com>
2024-08-08 15:08:19 +0200

    28

--- projects/europ-car-ui/src/lib/inputs/text/text.component.ts
+++ projects/europ-car-ui/src/lib/inputs/text/text.component.ts
@@ -1,13 +1,44 @@
-import { Component, ElementRef, EventEmitter, Input, NgModule, OnChanges, output, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, forwardRef, Input, NgModule, OnChanges, output, Output, ViewChild } from '@angular/core';
+import { ControlValueAccessor, DefaultValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'ecar-text',
   standalone: true,
   imports: [],
   templateUrl: './text.component.html',
-  styleUrl: './text.component.scss'
+  styleUrl: './text.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: TextComponent,
+      multi: true,
+    }
+  ]
 })
-export class TextComponent   {
+export class TextComponent implements ControlValueAccessor {
+
+ writeValue(value: any): void {
+ this.value= value;
+
+ if(this.valore){
+  this.valore.nativeElement.value = this.value;
+ }
+
+ console.log(value);
+}
+
+
+ registerOnTouched(fn: () => void): void {
+  console.log('touched');
+}
+
+registerOnChange(fn: (_: any) => {}): void {
+  console.log('change');
+}
+
+ setDisabledState(isDisabled: boolean): void {
+  console.log(isDisabled);
+}
 
 @ViewChild('valore') valore!: ElementRef<HTMLInputElement>
 
@@ -32,8 +63,6 @@ isDisabled:boolean = false;
 size:number = 30;
 
 
-onChange() {
-  this.valueChange.emit();
-}
+
 
 }
